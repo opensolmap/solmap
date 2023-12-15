ed3adeb339a890af79c41d669ebac8067cf6b310
Samuel Vanderwaal <samuel.vanderwaal@gmail.com>
2023-12-14 15:20:29 -0900

    regen client (#7)

--- src/client/errors/custom.ts
+++ src/client/errors/custom.ts
@@ -1,32 +1,48 @@
-export type CustomError = InvalidSolmapNumber | SolmapAlreadyMinted
+export type CustomError =
+  | MintingNotLiveYet
+  | InvalidSolmapNumber
+  | SolmapAlreadyMinted
 
-export class InvalidSolmapNumber extends Error {
+export class MintingNotLiveYet extends Error {
   static readonly code = 6000
   readonly code = 6000
+  readonly name = "MintingNotLiveYet"
+  readonly msg = "Minting is not live yet"
+
+  constructor(readonly logs?: string[]) {
+    super("6000: Minting is not live yet")
+  }
+}
+
+export class InvalidSolmapNumber extends Error {
+  static readonly code = 6001
+  readonly code = 6001
   readonly name = "InvalidSolmapNumber"
   readonly msg = "Invalid Solmap number"
 
   constructor(readonly logs?: string[]) {
-    super("6000: Invalid Solmap number")
+    super("6001: Invalid Solmap number")
   }
 }
 
 export class SolmapAlreadyMinted extends Error {
-  static readonly code = 6001
-  readonly code = 6001
+  static readonly code = 6002
+  readonly code = 6002
   readonly name = "SolmapAlreadyMinted"
   readonly msg = "Solmap already minted"
 
   constructor(readonly logs?: string[]) {
-    super("6001: Solmap already minted")
+    super("6002: Solmap already minted")
   }
 }
 
 export function fromCode(code: number, logs?: string[]): CustomError | null {
   switch (code) {
     case 6000:
-      return new InvalidSolmapNumber(logs)
+      return new MintingNotLiveYet(logs)
     case 6001:
+      return new InvalidSolmapNumber(logs)
+    case 6002:
       return new SolmapAlreadyMinted(logs)
   }
 
--- src/client/programId.ts
+++ src/client/programId.ts
@@ -1,9 +1,4 @@
 import { PublicKey } from "@solana/web3.js"
 
-// Program ID defined in the provided IDL. Do not edit, it will get overwritten.
-export const PROGRAM_ID_IDL = new PublicKey(
-  "9iSon7JwYNRF5UuCgvwucFRDSPGxLFuNCghr56q5khwR"
-)
-
 // This constant will not get overwritten on subsequent code generations and it's safe to modify it's value.
 export const PROGRAM_ID: PublicKey = PROGRAM_ID_IDL
