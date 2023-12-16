e7ca70e9f534612bfb0843ac54e7b968e6d03013
Samuel Vanderwaal <samuel.vanderwaal@magiceden.io>
2023-12-15 15:13:04 -0900

    update to new libreplex api

--- cli/src/commands/instructions.rs
+++ cli/src/commands/instructions.rs
@@ -38,9 +38,6 @@ pub fn create_mint_solmap_ix(authority: Pubkey, mint: Pubkey, solmap_number: u64
     let fvca = Pubkey::find_program_address(&["fvca".as_bytes()], &SOLMAP_PROGRAM_ID).0;
 
     let inscription_summary = find_inscription_summary_key();
-    let (inscription_ranks_current_page, inscription_ranks_next_page) =
-        find_inscription_rank_pages();
-    let inscription = find_inscription_key(mint);
     let inscription_v3 = find_inscription_v3_key(mint);
     let inscription_data = find_inscription_data_key(mint);
 
@@ -55,11 +52,8 @@ pub fn create_mint_solmap_ix(authority: Pubkey, mint: Pubkey, solmap_number: u64
             AccountMeta::new(metadata, false),
             AccountMeta::new(master_edition, false),
             AccountMeta::new_readonly(fvca, false),
-            AccountMeta::new(inscription, false),
             AccountMeta::new(inscription_v3, false),
             AccountMeta::new(inscription_data, false),
-            AccountMeta::new(inscription_ranks_current_page, false),
-            AccountMeta::new(inscription_ranks_next_page, false),
             AccountMeta::new(inscription_summary, false),
             AccountMeta::new_readonly(INSCRIPTION_PROGRAM_ID, false),
             AccountMeta::new_readonly(system_program::ID, false),
@@ -76,24 +70,6 @@ fn find_inscription_summary_key() -> Pubkey {
     let (pubkey, _) =
         Pubkey::find_program_address(&[b"inscription_summary"], &INSCRIPTION_PROGRAM_ID);
     pubkey
-}
-
-fn find_inscription_rank_pages() -> (Pubkey, Pubkey) {
-    let (current_page, _) = Pubkey::find_program_address(
-        &[b"inscription_rank", &[0, 0, 0, 0]],
-        &INSCRIPTION_PROGRAM_ID,
-    );
-    let (next_page, _) = Pubkey::find_program_address(
-        &[b"inscription_rank", &[1, 0, 0, 0]],
-        &INSCRIPTION_PROGRAM_ID,
-    );
-    (current_page, next_page)
-}
-
-fn find_inscription_key(mint: Pubkey) -> Pubkey {
-    let (key, _) =
-        Pubkey::find_program_address(&[b"inscription", &mint.to_bytes()], &INSCRIPTION_PROGRAM_ID);
-    key
 }
 
 fn find_inscription_v3_key(mint: Pubkey) -> Pubkey {
